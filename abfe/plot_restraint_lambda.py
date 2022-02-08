c8859c7ecdbe0a50dd298f38cde7d3062588a8d7
finlayclark <s1629668@ed.ac.uk>
2022-02-08 09:19:09 +0000

    Scripts modified for analysis of bound leg only

--- abfe/plot_restraint_lambda.py
+++ abfe/plot_restraint_lambda.py
@@ -1,10 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-k = 10 # kcal mol-1
+k = 40 # kcal mol-1
 
 def nrg(x,scale_fac=1):
-    return 0.5*k*scale_fac*x**2
+    return 0.5*k*(scale_fac*scale_fac)*abs(x)**(1+scale_fac)
 
 x = np.arange(-5,5,0.01)
 scale_facts = np.arange(0,1.1,0.1)
@@ -20,7 +20,8 @@ for scale_fac in scale_facts:
 
 ax.set_title(f"Scaling of Harmonic Distance Restraint with\nLambda for k = {k} kcal/ mol")
 ax.set_ylim(0,2.5)
+ax.set_xlim(-3,3)
 ax.legend()
 ax.axhline(y=0.593, color='r', linestyle='--')
-ax.text(-4.7, 0.65, "kT at 298 K", color='r')
-plt.savefig("scaled_restraint_energies.png")
+ax.text(0, 0.65, "kT at 298 K", color='r')
+plt.savefig("scaled_restraint_energies_not_sq.png")
